c8c9ca29ec1c2280d70de58fc9f75e1819e05a98
Chris Bowman <stereoabusestudios@gmail.com>
2024-11-12 16:06:29 -0500

    tests

--- src/band_name_tool.py
+++ src/band_name_tool.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import time
 from typing import Dict, List
 import re
---
+++ src/tests/conftest.py
@@ -0,0 +1,7 @@
+import sys
+from pathlib import Path
+
+# Add the src directory to Python path
+src_path = str(Path(__file__).parent.parent)
+if src_path not in sys.path:
+    sys.path.append(src_path)
